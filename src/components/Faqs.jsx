17748010c7aa2cf72c881c1503a4cf581d9c694e
asmaaHamdym <asmahamdym@gmail.com>
2024-06-11 11:12:44 +0300

    refactored HomePage component with new styles and layout

--- src/components/Faqs.jsx
+++
@@ -1,3 +0,0 @@
-export const Faqs = () => {
-  return <div>Faqs</div>;
-};
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,23 @@
 import { Events } from "./Events";
 import { Link } from "react-router-dom";
+import "../styles/Homepage.css";
 
 export const HomePage = () => {
   return (
     <>
       <h1>Event Listing</h1>
-      <p>
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam,
-        doloribus.
-      </p>
-      <div>
-        <button className="px-4 py-2 bg-[#412234] text-white font-semibold rounded-lg shadow-md">
-          <Link to="events">View All</Link>
-        </button>
+      <div className="events-header">
+        <p>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam,
+          doloribus.
+        </p>
+        <div>
+          <button className="px-4 py-2 bg-[#412234] text-white font-semibold rounded-lg shadow-md">
+            <Link to="events">
+              <span className="view-icon">+</span>View All
+            </Link>
+          </button>
+        </div>
       </div>
       <Events />
     </>
