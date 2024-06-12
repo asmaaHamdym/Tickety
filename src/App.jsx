5eb32d7304ffe95e1b6c75fab7eae502efd24ed3
asmaaHamdym <asmahamdym@gmail.com>
2024-06-12 17:15:39 +0300

    Updated App.jsx, Footer.jsx, HomePage.jsx, and styles files (index.css, Homepage.css, Card.css)

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { HomePage } from "./pages/HomePage";
 import { Events } from "./pages/Events";
+import { LogIn } from "./pages/LogIn";
 const router = createBrowserRouter([
   {
     path: "/",
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,20 +1,19 @@
 import { Events } from "./Events";
 import { Link } from "react-router-dom";
-
-// import Footer from "../components/Footer";
+import "../styles/Homepage.css";
 
 export const HomePage = () => {
   return (
     <>
       <div className="mx-12 my-4 py-8 pt-4 pb-0">
         <h1 className="text-3xl font-bold">Event Listing</h1>
-        <div className="text-xl font-bold flex justify-between">
+        <div className="text-xl font-bold flex justify-between sm:font-normal">
           <p>
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Nam,
             doloribus.
           </p>
           <div>
-            <button className="px-4 py-2 bg-[#412234] text-white font-semibold rounded-lg shadow-md">
+            <button className="px-4 py-2 bg-[#412234] text-white font-semibold rounded-lg shadow-md ">
               <Link to="events">View All</Link>
             </button>
           </div>
