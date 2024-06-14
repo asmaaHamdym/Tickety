22cc703d4037f9cf279fca864132e44fbe8cf9e4
asmaaHamdym <asmahamdym@gmail.com>
2024-06-14 13:09:44 +0300

    implemented NotFound page layout and styling

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -2,11 +2,21 @@ import { Link } from "react-router-dom";
 
 export const NotFound = () => {
   return (
-    <div>
-      404 Not Found!
-      <Link to="/" className="block text-lg text-purple-400 underline ">
-        Go Back to Home Page
-      </Link>
-    </div>
+    <>
+      <main className="bg-[#412234] w-full h-screen flex flex-col justify-center items-center">
+        <h1 className="mt-20 mb-4 p-4 text-9xl  text-white font-extrabold">
+          404
+        </h1>
+        <p className="font-bold  text-white mb-8">
+          WE ARE SORRY, BUT THE PAGE YOU REQUESTED WAS NOT FOUND
+        </p>
+        <Link
+          to="/"
+          className="block text-lg text-[#412234] bg-white rounded-3xl px-8 py-4"
+        >
+          Go Back to Homepage
+        </Link>
+      </main>
+    </>
   );
 };
