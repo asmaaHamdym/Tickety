a7c6f8b9bc01d2555af4d938169239a638e944fe
asmaaHamdym <asmahamdym@gmail.com>
2024-06-24 00:55:18 +0300

    "Added toggle password button to password input field in LogIn component"

--- src/pages/LogIn.jsx
+++ src/pages/LogIn.jsx
@@ -1,5 +1,7 @@
 import tickety from "../assets/tickety.png";
 import googleImg from "../assets/google.png";
+import togglePassword from "../assets/tooglePassword.png";
+
 import { Link } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { useState, useEffect } from "react";
@@ -121,16 +123,25 @@ export const LogIn = () => {
             >
               Password
             </label>
-            <input
-              type="password"
-              placeholder="Enter your password"
-              id="password"
-              name="password"
-              minLength={8}
-              value={formData.password}
-              className="border-2 p-4 w-full rounded-md"
-              onChange={handleChange}
-            />
+            <div className="flex border-2 p-4 w-full rounded-md justify-between">
+              <input
+                type="password"
+                placeholder="Enter your password"
+                id="password"
+                name="password"
+                minLength={8}
+                value={formData.password}
+                className=""
+                onChange={handleChange}
+              />
+              <button type="button">
+                <img
+                  src={togglePassword}
+                  alt="toggle password "
+                  className="w-6"
+                />
+              </button>
+            </div>
             <span className="text-[#E33629] m-4">{errors.password}</span>
 
             <div className="flex justify-between pb-12">
