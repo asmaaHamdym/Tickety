a74384be2f1b8b191e9a300a0c6a8d1d79ecda67
terejoe <nzeamalunkechinyere@gmail.com>
2024-07-09 10:13:05 +0100

    integrating hook form in input component

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Input = (props) => {
   return (
-    <input type={props.type} id={props.id} name={props.name} placeholder={props.placeholder} value={props.value} onChange={props.onChange} className={`${props.className} mt-1 px-3 py-2 bg-[#eaecee] border-2 shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm border-[#C4BAC0] placeholder:text-[#9FA7AF]`} />
+    <input type={props.type} id={props.id} name={props.name} placeholder={props.placeholder} {...register(label, {required})} className={`${props.className} mt-1 px-3 py-2 bg-[#eaecee] border-2 shadow-sm placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm border-[#C4BAC0] placeholder:text-[#9FA7AF]`} />
   )
 }
 export default Input;
