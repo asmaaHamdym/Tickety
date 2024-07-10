a710abc08b9e658108b40ac2c13f02e63508845d
asmaaHamdym <asmahamdym@gmail.com>
2024-07-11 00:12:41 +0300

    added Footer component to AboutUs page

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -2,6 +2,7 @@ import aboutImage from "../assets/about.png";
 import noteIcon from "../assets/note-icon.png";
 import reachIcon from "../assets/reach-icon.png";
 import ticketIcon from "../assets/ticket-icon.png";
+import Footer from "../components/Footer";
 import Navbar from "./Navbar";
 
 const AboutUs = () => {
@@ -15,8 +16,8 @@ const AboutUs = () => {
           </h3>
           <p className="text-sm text-[#586675] mb-10">
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
-            quaerat, quisquam labore minima excepturi, <br/>ducimus earum
-            maxime consectetur, enim unde nesciunt.{" "}
+            quaerat, quisquam labore minima excepturi, <br />
+            ducimus earum maxime consectetur, enim unde nesciunt.{" "}
           </p>
         </div>
 
@@ -32,15 +33,15 @@ const AboutUs = () => {
           <div className="mt-4 px-10">
             <div className="flex gap-10 border border-[#BDD9BF] rounded-md p-4 mb-10">
               <div>
-                <img src={noteIcon} alt="Note Icon" className="mt-2"/>
+                <img src={noteIcon} alt="Note Icon" className="mt-2" />
               </div>
               <div>
                 <p className="text-sm text-[#212D3A] font-semibold">
                   Create your personal event
                 </p>
                 <p className="text-sm text-[#586675] px-18">
-                  Organize a seamlesss experience for your customers <br/>at
-                  your event like never before.
+                  Organize a seamlesss experience for your customers <br />
+                  at your event like never before.
                 </p>
               </div>
             </div>
@@ -54,29 +55,31 @@ const AboutUs = () => {
                   Reach more Audience
                 </p>
                 <p className="text-sm text-[#586675] px-18">
-                  Reach new customersand get more audience <br/>to
-                  participate at your event.
+                  Reach new customersand get more audience <br />
+                  to participate at your event.
                 </p>
               </div>
             </div>
 
             <div className="flex gap-10 border border-[#BDD9BF] rounded-md p-4 mb-20">
               <div>
-                <img src={ticketIcon} alt="Ticket Icon" className="mt-2"/>
+                <img src={ticketIcon} alt="Ticket Icon" className="mt-2" />
               </div>
               <div>
                 <p className="text-sm text-[#212D3A] font-semibold">
                   Book a Ticket
                 </p>
                 <p className="text-sm text-[#586675] px-18">
-                  Get a ticket and RSVP for your favourite event<br/>with
-                  ease.
+                  Get a ticket and RSVP for your favourite event
+                  <br />
+                  with ease.
                 </p>
               </div>
             </div>
           </div>
         </div>
       </div>
+      <Footer />
     </div>
   );
 };
